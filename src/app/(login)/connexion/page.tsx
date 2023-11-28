a3835b7492e4435be4ba0cff733663831e4ce3ec
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-11-28 13:21:17 +0100

    Question 2.1

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -4,6 +4,7 @@ import {z} from 'zod';
 import {useForm, zodResolver} from '@mantine/form';
 import {NumberInput, TextInput, Button, Box, Group, PasswordInput} from '@mantine/core';
 import Link from "next/link";
+import {SectionContainer} from "tp-kit/components";
 
 const schema = z.object({
     name: z.string().min(2, {message: 'Name should have at least 2 letters'}),
@@ -22,28 +23,30 @@ export default function Inscription() {
     });
 
     return (
+        <SectionContainer wrapperClassName="max-w-5xl">
+            <Box maw={350} mx="auto" className="shadow-md my-5 bg-white rounded">
+                <form onSubmit={form.onSubmit((values) => console.log(values))} className="p-5">
+                    <h1 className="mb-3">CONNEXION</h1>
+                    <TextInput
+                        withAsterisk
+                        label="Adresse email"
+                        placeholder="example@mail.com"
+                        {...form.getInputProps('email')}
+                    />
+                    <PasswordInput
+                        withAsterisk
+                        label="Mot de passe"
+                        placeholder=""
+                        {...form.getInputProps('password')}
+                    />
 
-        <Box maw={350} mx="auto" className="shadow-md my-5">
-            <form onSubmit={form.onSubmit((values) => console.log(values))} className="p-5">
-                <h1 className="mb-3">CONNEXION</h1>
-                <TextInput
-                    withAsterisk
-                    label="Adresse email"
-                    placeholder="example@mail.com"
-                    {...form.getInputProps('email')}
-                />
-                <PasswordInput
-                    withAsterisk
-                    label="Mot de passe"
-                    placeholder=""
-                    {...form.getInputProps('password')}
-                />
-
-                <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600" fullWidth="true">
-                    Me connecter
-                </Button>
-            </form>
-            <Link href={'../inscription'} className="">Créer un compte</Link>
-        </Box>
+                    <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12"
+                            fullWidth="true">
+                        Me connecter
+                    </Button>
+                    <Link href={'../inscription'}><p className="text-sm text-center text-green">Créer un compte</p></Link>
+                </form>
+            </Box>
+        </SectionContainer>
     );
 }
--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -3,6 +3,8 @@
 import {z} from 'zod';
 import {useForm, zodResolver} from '@mantine/form';
 import {NumberInput, TextInput, Button, Box, Group, PasswordInput} from '@mantine/core';
+import {SectionContainer} from "tp-kit/components";
+import Link from "next/link";
 
 const schema = z.object({
     name: z.string().min(2, {message: 'Name should have at least 2 letters'}),
@@ -22,35 +24,38 @@ export default function Inscription() {
     });
 
     return (
+        <SectionContainer wrapperClassName="max-w-5xl">
+            <Box maw={350} mx="auto" className="shadow-md my-5 bg-white rounded">
+                <form onSubmit={form.onSubmit((values) => console.log(values))} className="p-5">
+                    <h1 className="mb-3">INSCRIPTION</h1>
+                    <TextInput
+                        withAsterisk
+                        label="Name"
+                        placeholder="John Doe"
+                        description="Le nom qui sera utilisé pour vos commandes"
+                        mt="sm"
+                        {...form.getInputProps('name')}
+                    />
+                    <TextInput
+                        withAsterisk
+                        label="Adresse email"
+                        placeholder="example@mail.com"
+                        {...form.getInputProps('email')}
+                    />
+                    <PasswordInput
+                        withAsterisk
+                        label="Mot de passe"
+                        placeholder=""
+                        {...form.getInputProps('password')}
+                    />
 
-        <Box maw={350} mx="auto" className="shadow-md my-5">
-            <form onSubmit={form.onSubmit((values) => console.log(values))} className="p-5">
-                <h1 className="mb-3">INSCRIPTION</h1>
-                <TextInput
-                    withAsterisk
-                    label="Name"
-                    placeholder="John Doe"
-                    description="Le nom qui sera utilisé pour vos commandes"
-                    mt="sm"
-                    {...form.getInputProps('name')}
-                />
-                <TextInput
-                    withAsterisk
-                    label="Adresse email"
-                    placeholder="example@mail.com"
-                    {...form.getInputProps('email')}
-                />
-                <PasswordInput
-                    withAsterisk
-                    label="Mot de passe"
-                    placeholder=""
-                    {...form.getInputProps('password')}
-                />
-
-                <Button type="submit" className="bg-green-600 my-5 h-75 items-center hover:bg-green-600" fullWidth="true">
-                    S'inscrire
-                </Button>
-            </form>
-        </Box>
+                    <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12"
+                            fullWidth="true">
+                        S'inscrire
+                    </Button>
+                    <Link href={'../connexion'}><p className="text-sm text-center text-green">Déjà un compte ? Se connecter</p></Link>
+                </form>
+            </Box>
+        </SectionContainer>
     );
 }
