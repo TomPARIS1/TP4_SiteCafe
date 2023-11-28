d6769861084a9caa44961fd72e592997c90ad538
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-11-28 14:51:55 +0100

    Question 2.2 & 2.3

---
+++ src/app/(login)/inscription/form.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import {z} from 'zod';
+import {useForm, zodResolver} from '@mantine/form';
+import {TextInput, Button, Box, PasswordInput} from '@mantine/core';
+import {NoticeMessage, SectionContainer} from "tp-kit/components";
+import Link from "next/link";
+import {useZodI18n, ZodI18nProvider} from "tp-kit/components/providers";
+import {useState} from "react";
+
+const schema = z.object({
+    name: z.string().min(2),
+    email: z.string().email().nonempty(),
+    password: z.string().min(6),
+});
+
+type FormValues = z.infer<typeof schema>;
+
+export const Form = function () {
+    // Applique les traductions à zod
+    useZodI18n(z);
+
+    const form = useForm<FormValues>({
+        validate: zodResolver(schema),
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+        },
+    });
+
+    const [notices, setNotices] = useState([]);
+
+    function Error(message) {
+        setNotices([...notices, {type: "error", message}]);
+    }
+
+    function Success(message) {
+        setNotices([...notices, {type: "success", message}]);
+    }
+
+
+    return (
+        <SectionContainer wrapperClassName="max-w-5xl">
+            <Box maw={350} mx="auto" className="shadow-md my-5 bg-white rounded">
+                <form onSubmit={form.onSubmit((values) => {
+                    if (values.name === 'error') {
+                        Error("Cette adresse n'est pas disponible");
+                    } else {
+                        Success("Votre inscription a bien été prise en compte. Validez votre adresse mail pour vous connecter");
+                    }
+                })} className="p-5">
+                        <h1 className="mb-3">INSCRIPTION</h1>
+
+                        {notices.map((notice, i) => (
+                            <NoticeMessage key={i}{...notice}/>
+                        ))}
+
+                        <TextInput
+                            withAsterisk
+                            label="Name"
+                            placeholder="John Doe"
+                            description="Le nom qui sera utilisé pour vos commandes"
+                            mt="sm"
+                            {...form.getInputProps('name')}
+                        />
+                        <TextInput
+                            withAsterisk
+                            label="Adresse email"
+                            placeholder="example@mail.com"
+                            {...form.getInputProps('email')}
+                        />
+                        <PasswordInput
+                            withAsterisk
+                            label="Mot de passe"
+                            placeholder=""
+                            {...form.getInputProps('password')}
+                        />
+
+                        <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12"
+                                fullWidth="true">
+                            S'inscrire
+                        </Button>
+                        <Link href={'../connexion'}><p className="text-sm text-center text-green">Déjà un compte ? Se
+                            connecter</p></Link>
+                    </form>
+            </Box>
+        </SectionContainer>
+    );
+};
