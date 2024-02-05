08bc8eb927e87c7ea3244c7c0da73d75ab522c9f
Tom <98466837+TomPARIS1@users.noreply.github.com>
2024-02-05 15:34:38 +0100

    Question 3.6

---
+++ src/app/api/rebuild-posts/route.ts
@@ -0,0 +1,18 @@
+import { revalidatePath } from "next/cache"
+import { NextResponse } from "next/server"
+
+export async function POST(req : Request) {
+    const keyWebhook = process.env.SUPABASE_WEBHOOK_KEY
+    const keyApi = req.headers.get("api-key")
+    if (keyApi === null) {
+        return new Response('"API-Key" header is missing', {
+            status: 401
+        })
+    } else if (keyApi !== keyWebhook) {
+        return new Response("API Key is not valid", {
+            status: 403
+        })
+    }
+    console.log("test")
+    
+}
